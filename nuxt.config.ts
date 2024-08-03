297e8e7bbd4689d6cb64bb583b02ec3868b57e6b
Ji Yeshang <81836540+CSBigCaptain@users.noreply.github.com>
2024-08-04 02:38:12 +0800

    更改Actions配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'nuxt_assets'
+  },
+  experimental: {
+    payloadExtraction: false
+  }
 })
