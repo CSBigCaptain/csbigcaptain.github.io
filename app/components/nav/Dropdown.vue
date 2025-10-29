27d1aa37360c606e30731f83f758149c0db3aaf5
Ji Yeshang <81836540+CSBigCaptain@users.noreply.github.com>
2025-10-29 20:08:46 +0800

    Add new file

---
+++ app/components/nav/Dropdown.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps(['trigger'])
+</script>
+
+<template>
+  <!-- 插槽与 mdui-drop-down 的相同 -->
+  <mdui-dropdown placement="auto" :trigger="props.trigger">
+    <slot name="default" />
+  </mdui-dropdown>
+</template>
+
+<style scoped></style>
